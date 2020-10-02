69bf90117b7b2cae12a1df34a51ba06c11be006c
Zehua-Chen <peterchen06242000@outlook.com>
2020-10-02 17:03:29 -0500

    [chore] Add Files

---
+++ Assets/FlappyBird/Modules/Movement/Scripts/Tests/MoveLeftTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using FlappyBird.Settings;
+
+namespace FlappyBird.Movement.Tests
+{
+    public class MoveLeftTests
+    {
+        [UnityTest]
+        public IEnumerator Test()
+        {
+            var gameObject = new GameObject();
+            gameObject.transform.position = new Vector3();
+            gameObject.name = "Test Move Left";
+
+            var rig = gameObject.AddComponent<Rigidbody2D>();
+            rig.bodyType = RigidbodyType2D.Kinematic;
+
+            var settings = ScriptableObject.CreateInstance<SpeedSettings>();
+
+            var moveLeft = gameObject.AddComponent<MoveLeft>();
+            moveLeft.SpeedSettings = settings;
+            moveLeft.Rigidbody2D = rig;
+
+            yield return new WaitForSeconds(1.0f);
+
+            Assert.Less(gameObject.transform.position.x, 0.0f);
+            Assert.True(Mathf.Approximately(0.0f, gameObject.transform.position.y));
+            Assert.True(Mathf.Approximately(0.0f, gameObject.transform.position.z));
+        }
+    }
+}
---
+++ Assets/FlappyBird/Modules/Settings/Scripts/SpeedSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace FlappyBird.Settings
+{
+    [CreateAssetMenu(fileName = "Speed", menuName = "Settings/Speed")]
+    public class SpeedSettings : ScriptableObject
+    {
+        [SerializeField]
+        float _worldSpeed = 100.0f;
+
+        public float WorldSpeed => _worldSpeed;
+    }
+}
